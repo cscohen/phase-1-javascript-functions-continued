ef9c3d780a8bcf574acce383da8b698baef4ebb8
Christopher Cohen <chris@cscohen.com>
2021-07-20 17:16:00 -0600

    commit tree ee05d8c61c6491f495eb5012f322b50f1c9fc4dd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(someFun = 'roller-skate') {
+    return `This Saturday, I want to ${someFun}!`
+}
+saturdayFun('bathe my dog')
+
+function mondayWork(notFun = 'go to the office') {
+    return `This Monday, I will ${notFun}.`
+}
+
+function wrapAdjective(flair = '*'){
+    return function(adjective = 'special'){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+const wrapSlash = wrapAdjective('slash')
+console.log(wrapSlash)
+console.log(wrapSlash('string'))
